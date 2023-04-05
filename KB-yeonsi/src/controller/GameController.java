b50f3f1b928d39515092f6c698e2af93b793cb01
woo yup Lee <79967044+leewooyup@users.noreply.github.com>
2023-04-05 19:31:52 +0900

    Update GameController.java

--- KB-yeonsi/src/controller/GameController.java
+++ KB-yeonsi/src/controller/GameController.java
@@ -2,23 +2,51 @@ package controller;
 
 import java.util.List;
 import exception.DMLException;
+import exception.SearchWrongException;
+import dto.AcademyEventDTO;
+import dto.AcademyOptionDTO;
 import service.GameService;
 import service.GameServiceImpl;
+import service.SystemService;
+import service.SystemServiceImpl;
 
 public class GameController {
 	private static GameService gameService = GameServiceImpl.getInstance();
 	
-	public static void optionSelectByEventId(int eventId) {
+	public static List<AcademyOptionDTO> optionSelectByEventId(int eventId) {
+		List<AcademyOptionDTO> list = null;
 		try {
-		  List<AcademyOptionDTO> list = gameService.optionSelectByEventId(eventId);
+		  list = service.optionSelectByEventId(eventId);
 		  
 		  for(int i = 0; i < list.size(); i++) {
+			  System.out.print(i+1 + ". ");
 			  System.out.println(list.get(i));
 		  }
 		  
 		} finally {
 			
 		}	
+		return list;
+	}
+	
+	public static void academyEventSelectByRandom() {
+		SystemService s = SystemServiceImpl.getInstance();
+		int size = s.selectAcademyEventAll().size();
+		int ran = (int)(Math.random() * size) + 1;
+		List<AcademyOptionDTO> list = null;
+		try {
+			AcademyEventDTO dto = service.academyEventSelectByRandom(ran);
+			System.out.println(dto);
+			list = optionSelectByEventId(ran);
+		} catch(SearchWrongException e) {
+			System.out.println("실패");
+		}
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("******고르세요*********");
+		int select = Integer.parseInt(scanner.nextLine());
+		System.out.println(list.get(select-1).getResultScript());
+		service.academyUpdate(list.get(select-1));
+//		AcademyOptionDTO selectedDto = service.optionSelectByOptionId
 	}
 
 
