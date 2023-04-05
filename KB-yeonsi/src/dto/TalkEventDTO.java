4e152f22a47349c8d2894b6b919c7faba75ed8db
Minji Kil <78344310+rninji@users.noreply.github.com>
2023-04-05 18:48:35 +0900

    Add TalkEventDTO

--- KB-yeonsi/src/dto/TalkEventDTO.java
+++ KB-yeonsi/src/dto/TalkEventDTO.java
@@ -1,5 +1,51 @@
 package dto;
 
-public class TalkEventDTO {
+import java.util.ArrayList;
+import java.util.List;
 
+public class TalkEventDTO {
+	private int eventId;
+	private String script;
+	private List<TalkOptionDTO> optionList = new ArrayList<>();
+	
+	public TalkEventDTO() {}
+
+	public int getEventId() {
+		return eventId;
+	}
+
+	public void setEventId(int eventId) {
+		this.eventId = eventId;
+	}
+
+	public String getScript() {
+		return script;
+	}
+
+	public void setScript(String script) {
+		this.script = script;
+	}
+
+	public List<TalkOptionDTO> getOptionList() {
+		return optionList;
+	}
+
+	public void setOptionList(List<TalkOptionDTO> optionList) {
+		this.optionList = optionList;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("TalkEventDTO [eventId=");
+		builder.append(eventId);
+		builder.append("\nscript\n:");
+		builder.append(script);
+		builder.append("\noptionList\n");
+		for (TalkOptionDTO option : optionList) {
+			builder.append(option.getOptionId() + "." + option.getSelName()+" ");
+		}
+		builder.append("]");
+		return builder.toString();
+	}
 }
---
+++ KB-yeonsi/src/dto/TalkOptionDTO.java
@@ -0,0 +1,90 @@
+package dto;
+
+public class TalkOptionDTO {
+	private int optionId;
+	private int eventId;
+	private String selName;
+	private String characterName;
+	private int likePoint;
+	private String resultScript;
+	
+	public TalkOptionDTO(){}
+
+	public TalkOptionDTO(int optionId, int eventId, String selName, String characterName, int likePoint,
+			String resultScript) {
+		super();
+		this.optionId = optionId;
+		this.eventId = eventId;
+		this.selName = selName;
+		this.characterName = characterName;
+		this.likePoint = likePoint;
+		this.resultScript = resultScript;
+	}
+
+	public int getOptionId() {
+		return optionId;
+	}
+
+	public void setOptionId(int optionId) {
+		this.optionId = optionId;
+	}
+
+	public int getEventId() {
+		return eventId;
+	}
+
+	public void setEventId(int eventId) {
+		this.eventId = eventId;
+	}
+
+	public String getSelName() {
+		return selName;
+	}
+
+	public void setSelName(String selName) {
+		this.selName = selName;
+	}
+
+	public String getCharacterName() {
+		return characterName;
+	}
+
+	public void setCharacterName(String characterName) {
+		this.characterName = characterName;
+	}
+
+	public int getLikePoint() {
+		return likePoint;
+	}
+
+	public void setLikePoint(int likePoint) {
+		this.likePoint = likePoint;
+	}
+
+	public String getResultScript() {
+		return resultScript;
+	}
+
+	public void setResultScript(String resultScript) {
+		this.resultScript = resultScript;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("TalkOptionDTO [optionId=");
+		builder.append(optionId);
+		builder.append(", eventId=");
+		builder.append(eventId);
+		builder.append(", selName=");
+		builder.append(selName);
+		builder.append(", characterName=");
+		builder.append(characterName);
+		builder.append(", likePoint=");
+		builder.append(likePoint);
+		builder.append(", resultScript=");
+		builder.append(resultScript);
+		builder.append("]");
+		return builder.toString();
+	}
+}
