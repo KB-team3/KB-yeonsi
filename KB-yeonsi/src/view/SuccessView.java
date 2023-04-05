28bd792e695150975f67a487c83b0528d455ea1b
lee5917 <127364682+lee5917@users.noreply.github.com>
2023-04-05 19:00:26 +0900

    Update SuccessView.java

--- KB-yeonsi/src/view/SuccessView.java
+++ KB-yeonsi/src/view/SuccessView.java
@@ -6,6 +6,8 @@ import dto.AcademyEventDTO;
 
 
 public class SuccessView {
+	static int cnt = 1;
+	
 	public static void printSelectAcademyEventAll(List<AcademyEventDTO> list) {
 		System.out.println("----AcademyEvent LIST ("+list.size()+") 개 ------------------");
 		for(AcademyEventDTO dto : list) {
@@ -24,17 +26,62 @@ public class SuccessView {
 		System.out.println(message);
 		
 	}
-	public static void rankingPrint(List<LikeDTO> list) {
-		for(LikeDTO dto : list)
-			System.out.println(dto);
-	
+	public static void rankingPrint(List<LikeDTO> list, int charNum) {
+		System.out.println(list.size());
+
+		switch (charNum) {
+		case 1:
+			System.out.println("우엽이의 랭킹을 출력합니다.");
+			for (LikeDTO dto : list) {
+				System.out.print((cnt++) + "위 |  ");
+				System.out.print(dto.getUserName() + "님에 대한 호감도는 ");
+				System.out.println(dto.getWooyeop());
+			}
+			break;
+
+		case 2:
+			System.out.println("상우의 랭킹을 출력합니다.");
+			for (LikeDTO dto : list) {
+				System.out.print((cnt++) + "위 |  ");
+				System.out.print(dto.getUserName() + "님에 대한 호감도는 ");
+				System.out.println(dto.getSangwoo());
+			}
+			break;
+		case 3:
+			System.out.println("선영이의 랭킹을 출력합니다.");
+			for (LikeDTO dto : list) {
+				System.out.print((cnt++) + "위 |  ");
+				System.out.print(dto.getUserName() + "님에 대한 호감도는 ");
+				System.out.println(dto.getSunyeong());
+			}
+			break;
+		case 4:
+			System.out.println("민지의 랭킹을 출력합니다.");
+			for (LikeDTO dto : list) {
+				System.out.print((cnt++) + "위 |  ");
+				System.out.print(dto.getUserName() + "님에 대한 호감도는 ");
+				System.out.println(dto.getMinji());
+			}
+			break;
+		case 5:
+			System.out.println("채림이의 랭킹을 출력합니다.");
+			for (LikeDTO dto : list) {
+				System.out.print((cnt++) + "위 |  ");
+				System.out.print(dto.getUserName() + "님에 대한 호감도는 ");
+				System.out.println(dto.getChaelim());
+			}
+			break;
+
+		}
+
 	}
 	
 	public static void goodEnding(String charName) {
-			System.out.println(charName + "님과 매칭되었습니다.");
-		
+		System.out.println(charName + "님과 매칭되었습니다.");
+
 	}
+
 	public static void badEnding() {
-		System.out.println("배드엔딩");
+		System.out.println("ㅜㅜ 누구와도 매칭되지 않았어요.");
 	}
 }
