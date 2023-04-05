ca7572a8b1a8060fd9456ddd4a4f4bf4a76652ec
Minji Kil <78344310+rninji@users.noreply.github.com>
2023-04-05 18:52:29 +0900

    Update SystemController.java

--- KB-yeonsi/src/controller/SystemController.java
+++ KB-yeonsi/src/controller/SystemController.java
@@ -3,29 +3,71 @@ package controller;
 import java.util.List;
 
 import dto.AcademyEventDTO;
+import dto.TalkEventDTO;
+import exception.DMLException;
+import exception.SearchWrongException;
 import service.SystemService;
 import service.SystemServiceImpl;
+import view.FailView;
 import view.SuccessView;
 
 public class SystemController {
 	public static SystemService systemService = SystemServiceImpl.getInstance();
 	
+	// 전체 조회
 	public static void selectAcademyEventAll() {
 		List<AcademyEventDTO> list = null;
 		try{
 			list = systemService.selectAcademyEventAll();
-		} catch (Exception e) {
-			e.printStackTrace();
+		} catch (SearchWrongException e) {
+			FailView.errorMessage(e.getMessage());
 		}
 		SuccessView.printSelectAcademyEventAll(list);
 	}
+	public static void selectTalkEventAll() {
+		List<TalkEventDTO> list = null;
+		try{
+			list = systemService.selectTalkEventAll();
+		} catch (SearchWrongException e) {
+			FailView.errorMessage(e.getMessage());
+		}
+		SuccessView.printSelectTalkEventAll(list);
+	}
 	
+	
+	// 등록
 	public static void insertAcademyEvent(AcademyEventDTO dto) {
 		try {
 			systemService.insertAcademyEvent(dto);
-		} catch (Exception e) {
-			e.printStackTrace();
+		} catch (DMLException e) {
+			FailView.errorMessage(e.getMessage());
 		}
-		System.out.println("등록 완료");
+		SuccessView.messagePrint("등록 완료");
+	}
+	public static void insertTalkEvent(TalkEventDTO dto) {
+		try {
+			systemService.insertTalkEvent(dto);
+		} catch (DMLException e) {
+			FailView.errorMessage(e.getMessage());
+		}
+		SuccessView.messagePrint("등록 완료");
+	}
+	
+
+	public static void deleteAcademyEvent(int no) {
+		try {
+			systemService.deleteAcademyEvent(no);
+		} catch (DMLException e) {
+			FailView.errorMessage(e.getMessage());
+		}
+		SuccessView.messagePrint("삭제 완료");
+	}
+	public static void deleteTalkEvent(int no) {
+		try {
+			systemService.deleteTalkEvent(no);
+		} catch (DMLException e) {
+			FailView.errorMessage(e.getMessage());
+		}
+		SuccessView.messagePrint("삭제 완료");
 	}
 }
