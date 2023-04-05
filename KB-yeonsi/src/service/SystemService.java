70b85a01fd60cbbd0e0e2227538082d4a40d966c
Minji Kil <78344310+rninji@users.noreply.github.com>
2023-04-05 18:53:04 +0900

    Update SystemService.java

--- KB-yeonsi/src/service/SystemService.java
+++ KB-yeonsi/src/service/SystemService.java
@@ -3,11 +3,15 @@ package service;
 import java.util.List;
 
 import dto.AcademyEventDTO;
+import dto.TalkEventDTO;
 
 public interface SystemService {
 
 	public List<AcademyEventDTO> selectAcademyEventAll();
 	public int insertAcademyEvent(AcademyEventDTO dto) ;
-	public int updateAcademyEvent() ;
-	public int deleteAcademyEvent();
+	public int deleteAcademyEvent(int no);
+	
+	public List<TalkEventDTO> selectTalkEventAll();
+	public int insertTalkEvent(TalkEventDTO dto) ;
+	public int deleteTalkEvent(int no);
 }
