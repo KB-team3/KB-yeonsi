3280ed4c4f9cb32fe63d7b5335d7757abba5c86e
woo yup Lee <79967044+leewooyup@users.noreply.github.com>
2023-04-05 19:35:15 +0900

    Update GameService.java

--- KB-yeonsi/src/service/GameService.java
+++ KB-yeonsi/src/service/GameService.java
@@ -10,4 +10,6 @@ public interface GameService {
 	void foodUpdate(String userName, String selectCharacter, int foodCode) throws DMLException;
 	void giftUpdate(String userName, String selectCharacter, int giftNum) throws DMLException;
 	List<AcademyOptionDTO> optionSelectByEventId(int eventId);
+	AcademyEventDTO academyEventSelectByRandom(int eventId);
+	int academyUpdate(AcademyOptionDTO dto);
 }
