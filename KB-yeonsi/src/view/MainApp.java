d9ec2f4a618fc1af85ffe7358afd02a255645473
SY <97646668+drsuneamer@users.noreply.github.com>
2023-04-05 19:54:44 +0900

    Update MainApp.java

--- KB-yeonsi/src/view/MainApp.java
+++ KB-yeonsi/src/view/MainApp.java
@@ -1,12 +1,34 @@
-package mvc.view;
+package view;
+
+import java.util.Scanner;
+
+import controller.GameController;
 
 public class MainApp {
-
+	static String userName = "";
 	public static void main(String[] args) {
-		//MenuView.menuChoice();
-		//EndingView.checkEnding();
-		EndingView.viewResult();
-
+		Scanner sc =new Scanner(System.in);
+		// MenuView.menuChoice();
+		// EndingView.checkEnding();
+		// EndingView.viewResult();
+		
+		System.out.println("KB아카데미에 오신걸 환영합니다 (●'◡'●')");
+		System.out.println("두근두근 당신의 짝꿍을 찾아볼까요?!");
+		System.out.println("당신의 이름을 입력해주세요!");
+		
+		userName = sc.nextLine();
+		
+		
+	
+		// ~~ 유저 인서트 ~~
+		
+		GameController.userInsert(MainApp.userName);
+		GameController.likeAbilityInsert(userName);
+		System.out.println("안녕하세요, " + userName + "!");
+		
+		// ~~ 유저 입력한 후
+		MenuView.menuChoice();
+		
 	}
 
 }
