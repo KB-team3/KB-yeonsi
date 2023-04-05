2c086f86719043f8370338249d9d5772ff5ed659
Minji Kil <78344310+rninji@users.noreply.github.com>
2023-04-05 18:53:34 +0900

    Update SystemView.java

--- KB-yeonsi/src/view/SystemView.java
+++ KB-yeonsi/src/view/SystemView.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 import controller.SystemController;
 import dto.AcademyEventDTO;
 import dto.AcademyOptionDTO;
+import dto.TalkEventDTO;
+import dto.TalkOptionDTO;
 
 public class SystemView {
 	public SystemView() {
@@ -19,69 +21,42 @@ public class SystemView {
 	 */
 	public void menuChoice() {
 		while(true) {
-       	 System.out.println("\n----------------------------------------");
+			System.out.println("\n---------------------------------------------------------------------------------------------------------------------------------------------------------");
        	 System.out.print("1. 아카데미 이벤트 전체 검색   ");
        	 System.out.print("2. 대화 이벤트 전체 검색   ");
-         	System.out.print("3. 메뉴 전체 검색   ");
-        	System.out.println("4. 선물 전체 검색   ");
         	
-        	System.out.print("5. 아카데미 이벤트 추가   ");
-        	System.out.print("6. 대화 이벤트 추가   ");
-        	System.out.print("7. 메뉴 추가   ");
-        	System.out.println("8. 선물 추가  ");
+        	System.out.print("3. 아카데미 이벤트 추가   ");
+        	System.out.print("4. 대화 이벤트 추가   ");
         	
-        	System.out.print("8. 아카데미 이벤트 수정  ");
-        	System.out.print("9. 대화 이벤트 수정  ");
-        	System.out.print("10. 메뉴 수정  ");
-        	System.out.println("11. 선물 수정  ");
+        	System.out.print("5. 아카데미 이벤트 삭제  ");
+        	System.out.print("6. 대화 이벤트 삭제  ");
         	
-        	System.out.print("12. 아카데미 이벤트 삭제  ");
-        	System.out.print("13. 대화 이벤트 삭제  ");
-        	System.out.print("14. 메뉴 삭제  ");
-        	System.out.println("15. 선물 삭제  ");
-        	
-        	System.out.print("16. 종료 ");
+        	System.out.print("7. 종료 ");
 
-             System.out.println("\n--------------------------------------------");
+             System.out.println("\n---------------------------------------------------------------------------------------------------------------------------------------------------------");
              System.out.println("선택메뉴는?");
              try {
 	              int menu = Integer.parseInt(sc.nextLine());//
 	              switch (menu) {
-				  case 1:
-					  // 아카데미 이벤트 전체 검색
+				  case 1: // 아카데미 이벤트 전체 검색
 					  SystemController.selectAcademyEventAll();
 					break;
-	               case 2:
+	               case 2: // 대화 이벤트 전체 검색
+	            	   SystemController.selectTalkEventAll();
 					break;
-	               case 3:
-	 				break;
-	               case 4:;
-	 				break;
-	               case 5: // 아카데미 이벤트 추가
+	               case 3: // 아카데미 이벤트 추가
 	            	   inputInsertAcademyEvent();
 	    				break;
-	               case 6:
+	               case 4: // 대화 이벤트 추가
+	            	   inputInsertTalkEvent();
 	    				break;
-	               case 7: 
-	    				break;
-	               case 8:
+	               case 5: // 아카데미 이벤트 삭제
+	            	   inputDeleteAcademyEvent();
 	    				break;	
-	               case 9:
+	               case 6: // 대화 이벤트 삭제
+	            	   inputDeleteTalkEvent();
 	    				break;	
-	               case 10:
-	    				break;	
-	               case 11:
-	    				break;	
-	               case 12:
-	    				break;	
-	               case 13:
-	    				break;	
-	               case 14:
-	    				break;	
-	               case 15:
-	    				break;	
-	    		
-	               case 16:
+	               case 7: // 종료하기
 	            	   // 현재는 종료로 되어 있는데 나중에는 홈으로 가는 걸로 바꾸는 게 나을듯
 	            	  System.out.println("종료합니다.");
 	    			 System.exit(0);	
@@ -97,7 +72,7 @@ public class SystemView {
 	}
 	
 	/**
-	 *  5. 아카데미 이벤트 추가
+	 *  3. 아카데미 이벤트 추가
 	 * @param args
 	 */
 	public static void inputInsertAcademyEvent() {
@@ -121,8 +96,53 @@ public class SystemView {
 		   	option.setResultScript(sc.nextLine());
 		   	list.add(option);
 	   	 }
+	   	 dto.setOptionList(list);
 	   	 SystemController.insertAcademyEvent(dto);
 	}
+	/**
+	 *  4. 대화 이벤트 추가
+	 * @param args
+	 */
+	public static void inputInsertTalkEvent() {
+		TalkEventDTO dto = new TalkEventDTO();
+		System.out.println("스크립트?");
+	   	 dto.setScript(sc.nextLine());
+	   	 
+	   	 System.out.println("선택지 개수?");
+	   	 int selCnt = Integer.parseInt(sc.nextLine());
+	   	 
+	   	 List<TalkOptionDTO> list = new ArrayList<>();
+	   	 for(int i = 0; i<selCnt; i++) {
+	   		TalkOptionDTO option = new TalkOptionDTO();
+	   		System.out.println("선택지 내용은?");
+	   		option.setSelName(sc.nextLine());
+		   	System.out.println("누구와의 대화인지?");
+		   	option.setCharacterName(sc.nextLine());
+		   	System.out.println("호감도 얼마나 오를 지?");
+		   	option.setLikePoint(Integer.parseInt(sc.nextLine())); ;
+		   	System.out.println("결과 스크립트는?");
+		   	option.setResultScript(sc.nextLine());
+		   	list.add(option);
+	   	 }
+	   	 dto.setOptionList(list);
+	   	 SystemController.insertTalkEvent(dto);
+	}
+	 /**
+     * 5. 아카데미 이벤트 삭제
+     * */
+     public static void inputDeleteAcademyEvent() {
+    	 System.out.println("삭제할 아카데미 이벤트 번호는?");
+    	 int no = Integer.parseInt(sc.nextLine());
+    	SystemController.deleteAcademyEvent(no);
+     }
+     /**
+      * 6. 대화 이벤트 삭제
+      * */
+      public static void inputDeleteTalkEvent() {
+     	 System.out.println("삭제할 대화 이벤트 번호는?");
+     	 int no = Integer.parseInt(sc.nextLine());
+     	SystemController.deleteTalkEvent(no);
+      }
 	
 	public static void main(String [] args) {
 		SystemView sv = new SystemView();
