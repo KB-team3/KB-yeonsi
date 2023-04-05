0e2dbe14e40dc4b890173e9524d3332f2b7c259c
lee5917 <127364682+lee5917@users.noreply.github.com>
2023-04-05 16:21:15 +0900

    feat: add ending, prints in successview

--- KB-yeonsi/src/view/SuccessView.java
+++ KB-yeonsi/src/view/SuccessView.java
@@ -13,4 +13,22 @@ public class SuccessView {
 		}
 		
 	}
+	
+	public static void messagePrint(String message) {
+		System.out.println(message);
+		
+	}
+	public static void rankingPrint(List<LikeDTO> list) {
+		for(LikeDTO dto : list)
+			System.out.println(dto);
+	
+	}
+	
+	public static void goodEnding(String charName) {
+			System.out.println(charName + "님과 매칭되었습니다.");
+		
+	}
+	public static void badEnding() {
+		System.out.println("배드엔딩");
+	}
 }
