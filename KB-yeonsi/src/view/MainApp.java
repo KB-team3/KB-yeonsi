17c1d351ac792b63abf97cabbc36b2b8924e90eb
SY <97646668+drsuneamer@users.noreply.github.com>
2023-04-06 14:16:14 +0900

    edit comment

--- KB-yeonsi/src/view/MainApp.java
+++ KB-yeonsi/src/view/MainApp.java
@@ -16,17 +16,16 @@ public class MainApp {
 		System.out.println("두근두근 당신의 짝꿍을 찾아볼까요?!");
 		System.out.println("당신의 이름을 입력해주세요!");
 		
+		// 유저 이름 입력 받음
 		userName = sc.next();
-		
 		if (userName.equals("system")) new SystemView();
 	
-		// ~~ 유저 인서트 ~~
-		
+		// 유저 이름 입력 받은 후 DB에 user, 연관된 likeability insert
 		GameController.userInsert(MainApp.userName);
 		GameController.likeAbilityInsert(userName);
 		System.out.println("안녕하세요, " + userName + "!");
 		
-		// ~~ 유저 입력한 후
+		// 유저 입력한 후 menuView로 이동
 		new MenuView();
 		
 	}
